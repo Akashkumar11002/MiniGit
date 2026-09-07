19419e5d90bdeb958442c1e634379f6fd31c1906
Akash Kumar <akashk5638897@gmail.com>
2026-09-07 08:13:00 +0530

    initialize the code

---
+++ build/CMakeFiles/minigit.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/minigit.dir/src/main.cpp.o"
+  "CMakeFiles/minigit.dir/src/main.cpp.o.d"
+  "minigit"
+  "minigit.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/minigit.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
